a9fe1bb2d99d615f77589270dd7af654679a40d5
itsAkito <jayantKumar@gmail.com>
2025-11-27 16:43:41 +0530

    Add full project code without deletions

--- src/components/Hero/HeroSection.jsx
+++ src/components/Hero/HeroSection.jsx
@@ -1,23 +1,3 @@
-// import React from "react"
-// import"./Hero.css"
-// import AnchorLink from "react-anchor-link-smooth-scroll"
-// const Hero=()=>{
-//     return(
-//         <div id="home" className="hero">
-//             <h1>Hello! <span>I'm Jayant Kumar,</span>  fullstack developer based in india</h1>
-//             <p> I am a fullstack developer from Uttar Pradesh, India </p>
-//             <div className="hero-action">
-//                 <div className="hero-connect"><AnchorLink className="anchor-link" offset={50} href="#contact">
-//                             <p onClick={() => SetMenu("contact")}></p>Connect With Me
-//                           </AnchorLink></div>
-//                 <div className="hero-resume">My resume</div>
-//             </div>
-
-
-//         </div>
-//     )
-// }
-// export default Hero
 import React from "react";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 
